bdafd64b9af9a502f7326fa55bd9b4eb678ff511
crgimenes <crg@crg.eti.br>
2024-11-09 15:00:37 -0300

    simplifies protocol

--- protocol/protocol_test.go
+++ protocol/protocol_test.go
@@ -8,45 +8,17 @@ import (
 	"angus/constants"
 )
 
-func Test_checksum(t *testing.T) {
-	type args struct {
-		data []byte
-	}
-	tests := []struct {
-		name string
-		args args
-		want uint32
-	}{
-		{"empty", args{[]byte{}}, 2166136261},
-		{"a", args{[]byte{'a'}}, 3826002220},
-		{"ab", args{[]byte{'a', 'b'}}, 1294271946},
-		{"abc", args{[]byte{'a', 'b', 'c'}}, 440920331},
-		{"abcd", args{[]byte{'a', 'b', 'c', 'd'}}, 3459545533},
-		{"abcde", args{[]byte{'a', 'b', 'c', 'd', 'e'}}, 1956368136},
-		{"óüçã", args{[]byte("óüçã")}, 3909595796},
-		{"⠁", args{[]byte("\u2801")}, 943521466},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := checksum(tt.args.data); got != tt.want {
-				t.Errorf("checksum(%v) = %v, want %v",
-					string(tt.args.data), got, tt.want)
-			}
-		})
-	}
-}
-
 func TestEncodeDecode(t *testing.T) {
 	in := []byte("hello")
 	out := make([]byte, MaxPackageSize)
 
-	nout, err := Encode(out, in, 0x01, 0x01)
+	nout, err := Encode(out, in, 0x01)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	data := make([]byte, constants.BufferSize)
-	cmd, n, _, err := Decode(data, out[:nout])
+	cmd, n, err := Decode(data, out[:nout])
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -61,85 +33,78 @@ func TestEncodeDecode(t *testing.T) {
 		t.Errorf("data = %q, want %q", string(data[:n]), string(in))
 	}
 
-	// test invalid checksum
-	out[7] = 0x00
-	_, _, _, err = Decode(data, out[:nout])
-	if err != ErrInvalidChecksum {
-		t.Errorf("err = %v, want %v", err, ErrInvalidChecksum)
-	}
-
 	// test invalid size
-	_, _, _, err = Decode(data, out[:nout-1])
+	_, _, err = Decode(data, out[:nout-1])
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, constants.BufferSize+1), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, constants.BufferSize+1), 0x01)
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, 0), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, 0), 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, constants.BufferSize), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, constants.BufferSize), 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, constants.BufferSize-1), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, constants.BufferSize-1), 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, 1), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, 1), 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, 0), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, 0), 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// test invalid size
-	_, err = Encode(out, make([]byte, MaxPackageSize+1), 0x01, 0x01)
+	_, err = Encode(out, make([]byte, MaxPackageSize+1), 0x01)
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
 
 	// decode invalid size
-	_, _, _, err = Decode(data, make([]byte, 1))
+	_, _, err = Decode(data, make([]byte, 1))
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
 
 	// decode invalid size
-	_, _, _, err = Decode(data, make([]byte, 0))
+	_, _, err = Decode(data, make([]byte, 0))
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
 
 	data = []byte(randonPayload(100))
-	_, err = Encode(out, data, 0x01, 0x01)
+	_, err = Encode(out, data, 0x01)
 	if err != nil {
 		t.Errorf("err = %v, want nil", err)
 	}
 
 	// change size in data to FFFFFFFF
+	out[2] = 0xFF
 	out[3] = 0xFF
 	out[4] = 0xFF
 	out[5] = 0xFF
-	out[6] = 0xFF
-	_, _, _, err = Decode(data, out)
+	_, _, err = Decode(data, out)
 	if err != ErrInvalidSize {
 		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
 	}
@@ -160,36 +125,26 @@ func randonPayload(n int) string {
 
 func TestEncodeDecodeLoop(t *testing.T) {
 	data := make([]byte, constants.BufferSize)
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 255; i++ {
 		in := []byte(randonPayload(rand.Intn(10 + i)))
 		out := make([]byte, MaxPackageSize)
 
-		n, err := Encode(out, in, 0x01, uint16(i))
+		n, err := Encode(out, in, uint8(i))
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		cmd, n, counter, err := Decode(data, out[:n])
+		cmd, n, err := Decode(data, out[:n])
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if cmd != 0x01 {
-			t.Errorf("cmd = %v, want 0x01", cmd)
+		if cmd != uint8(i) {
+			t.Errorf("cmd = %v, want %v", cmd, i)
 		}
 
 		if string(data[:n]) != string(in) {
 			t.Errorf("data = %v, want %v", string(data), string(in))
-		}
-
-		// test counter
-		if int(counter) != i {
-			t.Errorf("counter = %v, want %v", counter, i)
-		}
-
-		// test checksum
-		if checksum(in) != checksum(data[:n]) {
-			t.Errorf("checksum = %v, want %v", checksum(in), checksum(data))
 		}
 
 		// test size
@@ -205,26 +160,26 @@ func ExampleEncode() {
 	data := []byte("hello")
 	out := make([]byte, MaxPackageSize)
 
-	n, err := Encode(out, data, 0x01, 0x01)
+	n, err := Encode(out, data, 0x01)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("data: %02X\n", string(out[:n]))
 	// Output:
-	// data: 0100010000000568656C6C6F7CE86368
+	// data: 010000000568656C6C6F
 }
 
 func ExampleDecode() {
 	data := []byte("hello")
 	out := make([]byte, MaxPackageSize)
 
-	n, err := Encode(out, data, 0x01, 0x01)
+	n, err := Encode(out, data, 0x01)
 	if err != nil {
 		panic(err)
 	}
 
-	cmd, n, _, err := Decode(data, out[:n])
+	cmd, n, err := Decode(data, out[:n])
 	if err != nil {
 		panic(err)
 	}
